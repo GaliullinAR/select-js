239c61c4f06787559bf49154db1d4dcfe5b88160
GaliullinAR <galiullin.ar651@gmail.com>
2023-02-08 13:02:29 +0300

    добавил функционал выбора селекста

--- classes/select.js
+++ classes/select.js
@@ -1,17 +1,24 @@
-const getTemplate = () => {
+const getTemplate = (data = [], placeholder, selectedId) => {
+  let text = placeholder ?? 'Placeholder по умолчанию';
+
+  const items = data.map(item => {
+    if (item.id === selectedId) {
+      text = item.value;
+    }
+
+    return `
+      <li class="select__item" data-type="item" data-id="${item.id}">${item.value}</li>    
+    `;
+  })
+
   return `
-    <div class="select__input">
-      <span>Hello select</span> 
-      <i class="fa-solid fa-caret-down"></i>
+    <div class="select__input" data-type="input">
+      <span data-type="value">${text}</span> 
+      <i class="fa-solid fa-caret-down" data-type="arrow"></i>
     </div>
     <div class="select__dropdown">
       <ul class="select__list">
-        <li class="select__item">123</li>
-        <li class="select__item">123</li>
-        <li class="select__item">123</li>
-        <li class="select__item">123</li>
-        <li class="select__item">123</li>
-        <li class="select__item">123</li>
+        ${items.join('')}
       </ul>
     </div>
   `;
@@ -21,17 +28,74 @@ export class Select {
   constructor(selector, options) {
     this.selector = document.querySelector(selector);
     this.options = options;
+    this.selectedId = options.selectedId;
+
+    this.#render();
+    this.#setup();
+  }
+
+  #render() {
+    const { placeholder, data } = this.options;
+    this.selector.classList.add('select');
+    this.selector.insertAdjacentHTML('beforeend', getTemplate(data, placeholder, this.selectedId));
+  }
+
+  #setup() {
+    this.clickHandler = this.clickHandler.bind(this);
+    this.selector.addEventListener('click', this.clickHandler);
+    this.arrow = this.selector.querySelector('[data-type="arrow"]');
+    this.value = this.selector.querySelector('[data-type="value"]');
+  }
+
+  clickHandler(event) {
+    const { type } = event.target.dataset;
+
+    if (type === 'input') {
+      this.toggle();
+    } else if (type === 'item') {
+      const id = event.target.dataset.id;
+      this.select(id);
+    }
+  }
+
+  get isOpen() {
+    return this.selector.classList.contains('open');
+  }
+
+  get current() {
+    return this.options.data.find(item => item.id === this.selectedId);
+  }
+
+  select(id) {
+    this.selectedId = Number(id);
+    this.value.textContent = this.current.value;
+
+    this.selector.querySelectorAll('[data-type="item"]').forEach(item => {
+      item.classList.remove('selected');
+    })
+    this.selector.querySelector(`[data-id="${id}"]`).classList.add('selected');
+
+    this.close();
+  }
+
+  toggle() {
+    this.isOpen ? this.close() : this.open();
   }
 
   open() {
     this.selector.classList.add('open');
-    if (this.selector.classList.contains('open')) return true;
+    this.arrow.classList.remove('fa-caret-down');
+    this.arrow.classList.add('fa-caret-up');
+    if (this.selector.classList.contains('open')) true;
     return false;
   }
 
   close() {
     this.selector.classList.remove('open');
-    if (!this.selector.classList.contains("open")) return true;
+    this.arrow.classList.add('fa-caret-down');
+    this.arrow.classList.remove('fa-caret-up');
+    if (!this.selector.classList.contains("open")) true;
     return false;
   }
+
 }
--- index.js
+++ index.js
@@ -2,7 +2,15 @@ import { Select } from "./classes/select";
 import './css/style.scss';
 
 const select = new Select('#select', {
-
+  placeholder: 'Выбери элемент',
+  selectedId: 1,
+  data: [
+    { id: 1, value: 'React' },
+    { id: 2, value: 'Angular' },
+    { id: 3, value: 'Vue' },
+    { id: 4, value: 'React Native' },
+    { id: 5, value: 'Next' },
+    {id: 6, value: 'Nest'}
+  ]
 });
 
-window.s = select
